13c4f69b6f71b4a986ef363e6188283c506c7d20
HARSHITB-0 <beniwalhappy5107@gmail.com>
2025-11-04 23:30:11 +0530

    Create exp_6.c

---
+++ exp_6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int num, rev_num, rem;
+    rev_num = 0;
+    printf("Enter the no. to be revrsed \n");
+    scanf("%d", &num);
+    do
+    {
+        rem = num % 10;
+        rev_num = rem + (rev_num * 10);
+        num = num / 10;
+    } while (num != 0);
+    printf("The reversed no. is : %d \n", rev_num);
+    return 0;
+}
