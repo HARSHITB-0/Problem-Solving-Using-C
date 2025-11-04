0eba9d99325b89b935866776545fb1eb67fe4e91
HARSHITB-0 <beniwalhappy5107@gmail.com>
2025-11-04 18:14:25 +0530

    Create exp_1.c

---
+++ exp_1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    printf("Name : Harshit Beniwal\nRoll no. : 125113054");
+    int a;
+    printf("Please enter a positive integer: \n");
+    scanf("%d", &a);
+    while (a<0)
+    {
+        /* code */
+        printf("please re enter a positive integer:\n");
+        scanf("%d", &a);
+    }
+    
+    if (a%2==0)
+    {
+        printf("The entered no. %d is an even no.", a);
+    }
+    else 
+    printf("The entered no. %d is an odd no.", a);
+    return 0;
+}
